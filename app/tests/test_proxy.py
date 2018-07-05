0bf5fdc6ce7993e2c962a00c945f8b01dc736b20
3C111 <digital_urn@hotmail.com>
2018-07-05 13:26:42 +0200

    Added some missing headers

--- app/tests/test_proxy.py
+++ app/tests/test_proxy.py
@@ -1,3 +1,23 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2018 - Swiss Data Science Center (SDSC)
+# A partnership between École Polytechnique Fédérale de Lausanne (EPFL) and
+# Eidgenössische Technische Hochschule Zürich (ETHZ).
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+""" Test for the proxy """
+
+
 import pytest
 from .. import app
 import responses
@@ -65,5 +85,5 @@ def test_passthrough_happyflow(client):
 
     rv = client.get('api/gitlab/v4/projects/', headers=headers)
 
-    #assert rv.status_code == 200
-    #assert b'No authorization header found' not in rv.data
+    assert rv.status_code == 200
+    assert b'No authorization header found' not in rv.data
