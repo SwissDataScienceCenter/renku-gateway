ddafc03246754c4305e18796f0aa967c35e3acc9
Andreas Bleuler <andreas.bleuler@sdsc.ethz.ch>
2018-08-06 17:49:17 +0200

    Use telepresence for local development. Make it easily accessible through `make dev`.

---
+++ run-telepresence.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# Copyright 2018 - Swiss Data Science Center (SDSC)
+# A partnership between École Polytechnique Fédérale de Lausanne (EPFL) and
+# Eidgenössische Technische Hochschule Zürich (ETHZ).
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+MINIKUBE_IP=`minikube ip`
+CURRENT_CONTEXT=`kubectl config current-context`
+
+echo "You are going to exchange k8s deployments using the following context: ${CURRENT_CONTEXT}"
+read -p "Do you want to proceed? [y/n]"
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 1
+fi
+FLASK_DEBUG=1 \
+HOST_NAME=http://${MINIKUBE_IP} \
+RENKU_ENDPOINT=http://${MINIKUBE_IP} \
+GITLAB_URL=http://${MINIKUBE_IP}/gitlab \
+KEYCLOAK_URL=http://${MINIKUBE_IP} \
+GATEWAY_SERVICE_PREFIX=/api/ \
+telepresence --swap-deployment renku-gateway --namespace renku --expose 5000 --run pipenv run python run.py
