06ff27cbdc7ba7f5bc7cfbf235c6e643042faecd
Tasko Olevski <olevski90@gmail.com>
2023-03-02 15:43:48 +0100

    feat(app): sticky sessions middleware (#630)

---
+++ internal/stickysessions/main.go
@@ -0,0 +1,198 @@
+// Package stickysessions implements a sticky session load balancer for echo's reverse proxy middleware.
+package stickysessions
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"path/filepath"
+	"time"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	discoveryV1 "k8s.io/api/discovery/v1"
+	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+)
+
+type K8sEndpointSliceWatcher interface {
+	AddEventHandler(cache.ResourceEventHandler) (cache.ResourceEventHandlerRegistration, error)
+}
+
+type K8sEndpointSliceLister interface {
+	List(labels.Selector) ([]*discoveryV1.EndpointSlice, error)
+}
+
+type StickySessionBalancer struct {
+	Service           string
+	Namespace         string
+	ContainerPortName string
+	CookieName        string
+	store             *EndpointStore
+	cache             K8sEndpointSliceLister
+	watcher           K8sEndpointSliceWatcher
+}
+
+func NewStickySessionBalancer(service string, namespace string, containerPortName string) middleware.ProxyBalancer {
+	var clientConfig *rest.Config
+	clientConfig, err := rest.InClusterConfig()
+	if err != nil {
+		log.Println("Cannot find in-cluster config, looking for kubeconfig file")
+		var kubeconfigPath string
+		if home := homedir.HomeDir(); home != "" {
+			kubeconfigPath = filepath.Join(home, ".kube", "config")
+		}
+		flag.Parse()
+
+		clientConfig, err = clientcmd.BuildConfigFromFlags("", kubeconfigPath)
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+
+	clientset, err := kubernetes.NewForConfig(clientConfig)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	listOptions := func(options *metaV1.ListOptions) {
+		options.LabelSelector = fmt.Sprintf("kubernetes.io/service-name=%s", service)
+	}
+	factory := informers.NewSharedInformerFactoryWithOptions(clientset, time.Second*30, informers.WithNamespace(namespace), informers.WithTweakListOptions(listOptions))
+	endpointInformer := factory.Discovery().V1().EndpointSlices()
+	factory.Start(ctx.Done())
+	synced := factory.WaitForCacheSync(ctx.Done())
+	for v, ok := range synced {
+		if !ok {
+			log.Fatalf("caches failed to sync: %v", v)
+		}
+	}
+
+	slices, err := endpointInformer.Lister().List(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	endpointStore := NewEndpointStoreFromEndpointSlices(slices, containerPortName)
+
+	cache := endpointInformer.Lister()
+	watcher := endpointInformer.Informer()
+	balancer := StickySessionBalancer{
+		Service:           service,
+		Namespace:         namespace,
+		ContainerPortName: containerPortName,
+		CookieName:        "reverse-proxy-sticky-session",
+		store:             endpointStore,
+		cache:             cache,
+		watcher:           watcher,
+	}
+	balancer.watcher.AddEventHandler(balancer)
+	return &balancer
+}
+
+// AddTarget is required by the LoadBalancer interface.
+// It is a no-op because the list of proxy targets is managed by a k8s event listener.
+func (s *StickySessionBalancer) AddTarget(target *middleware.ProxyTarget) bool {
+	return true
+}
+
+// RemoveTarget is required by the LoadBalancer interface.
+// It is a no-op because the list of proxy targets is managed by a k8s event listener.
+func (s *StickySessionBalancer) RemoveTarget(name string) bool {
+	return true
+}
+
+// Next assigns a proxy target to every incoming request that is received by the proxy
+// and the load balancer.
+func (s *StickySessionBalancer) Next(c echo.Context) *middleware.ProxyTarget {
+	var upstream EndpointStoreItem
+	var upstreamFound bool
+	var cookie *http.Cookie
+	cookie, err := c.Request().Cookie(s.CookieName)
+	if err == http.ErrNoCookie {
+		// The cookie does not exist at all
+		upstream, found := s.store.Peek()
+		if !found {
+			c.String(http.StatusNotFound, "no upstream servers are available")
+		}
+		s.store.IncrementSessions(upstream.UID, 1)
+		cookie = &http.Cookie{
+			Name:  s.CookieName,
+			Value: upstream.UID,
+		}
+		c.SetCookie(cookie)
+		return upstream.ProxyTarget()
+	}
+	upstream, upstreamFound = s.store.Get(cookie.Value)
+	if !upstreamFound {
+		// The cookie exists but is pointing to an upstream that does not exist
+		upstream, upstreamFound = s.store.Peek()
+		if !upstreamFound {
+			c.String(http.StatusNotFound, "no upstream servers are available")
+		}
+		s.store.IncrementSessions(upstream.UID, 1)
+		cookie = &http.Cookie{
+			Name:  s.CookieName,
+			Value: upstream.UID,
+		}
+		c.SetCookie(cookie)
+	}
+	return upstream.ProxyTarget()
+}
+
+func (s StickySessionBalancer) OnAdd(obj interface{}) {
+	slice := obj.(*discoveryV1.EndpointSlice)
+	endpointItems := NewEndpointStoreItems(slice, s.ContainerPortName)
+	for _, endpointItem := range endpointItems {
+		if s.store.Exists(endpointItem.UID) || !endpointItem.Ready {
+			continue
+		}
+		s.store.Add(endpointItem)
+	}
+}
+
+func (s StickySessionBalancer) OnUpdate(oldObj, newObj interface{}) {
+	newStore := NewEndpointStoreFromEndpointItems(NewEndpointStoreItems(newObj.(*discoveryV1.EndpointSlice), s.ContainerPortName), true)
+	oldStore := NewEndpointStoreFromEndpointItems(NewEndpointStoreItems(newObj.(*discoveryV1.EndpointSlice), s.ContainerPortName), true)
+	for _, newItem := range newStore.List() {
+		if !newItem.Ready {
+			// An endpoint is not ready to receive traffic, remove from store
+			s.store.Remove(newItem.UID)
+			continue
+		}
+		oldItem, oldItemFound := oldStore.Get(newItem.UID)
+		if !oldItemFound {
+			// A new endpoint has been added the slice
+			s.store.Add(newItem)
+			continue
+		}
+		if oldItem.Host != newItem.Host {
+			// An existing endpoint has changed its address
+			s.store.UpdateHost(newItem.UID, newItem.Host)
+		}
+	}
+	for _, oldItem := range oldStore.List() {
+		_, found := newStore.Get(oldItem.UID)
+		if !found {
+			// An old endpoiint has been removed
+			s.store.Remove(oldItem.UID)
+		}
+	}
+}
+
+func (s StickySessionBalancer) OnDelete(obj interface{}) {
+	removedSlice := obj.(*discoveryV1.EndpointSlice)
+	for _, endpoint := range removedSlice.Endpoints {
+		uid := string(endpoint.TargetRef.UID)
+		s.store.Remove(uid)
+	}
+}
---
+++ internal/stickysessions/main_test.go
@@ -0,0 +1,415 @@
+package stickysessions
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strconv"
+	"sync"
+	"testing"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/stretchr/testify/assert"
+	coreV1 "k8s.io/api/core/v1"
+	discoveryV1 "k8s.io/api/discovery/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/tools/cache"
+)
+
+type MockInformer struct {
+	store        []*discoveryV1.EndpointSlice
+	eventHandler cache.ResourceEventHandler
+	mutex        sync.RWMutex
+}
+
+func (m *MockInformer) List(labels.Selector) ([]*discoveryV1.EndpointSlice, error) {
+	m.mutex.RLock()
+	defer m.mutex.RUnlock()
+	return m.store, nil
+}
+
+func (m *MockInformer) TriggerAdd(obj *discoveryV1.EndpointSlice) {
+	func() {
+		m.mutex.Lock()
+		defer m.mutex.Unlock()
+		m.store = append(m.store, obj)
+	}()
+	m.eventHandler.OnAdd(obj)
+}
+
+func (m *MockInformer) TriggerDelete(obj *discoveryV1.EndpointSlice) {
+	for i, islice := range m.store {
+		if islice.Endpoints[0].TargetRef.UID == obj.Endpoints[0].TargetRef.UID {
+			func() {
+				m.mutex.Lock()
+				defer m.mutex.Unlock()
+				newStore := []*discoveryV1.EndpointSlice{}
+				newStore = append(newStore, m.store[:i]...)
+				if i < len(m.store)-1 {
+					newStore = append(newStore, m.store[i+1:]...)
+				}
+				m.store = newStore
+			}()
+			m.eventHandler.OnDelete(obj)
+			return
+		}
+	}
+}
+
+func (m *MockInformer) TriggerUpdate(oldObj, newObj *discoveryV1.EndpointSlice) {
+	for i, islice := range m.store {
+		if islice.Endpoints[0].TargetRef.UID == oldObj.Endpoints[0].TargetRef.UID {
+			func() {
+				m.mutex.Lock()
+				defer m.mutex.Unlock()
+				m.store[i] = newObj
+			}()
+			m.eventHandler.OnUpdate(oldObj, newObj)
+			return
+		}
+	}
+}
+
+func (m *MockInformer) AddEventHandler(handler cache.ResourceEventHandler) (cache.ResourceEventHandlerRegistration, error) {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+	m.eventHandler = handler
+	return nil, nil
+}
+
+func setupTestUpstream(ID string) (*httptest.Server, *url.URL, discoveryV1.EndpointSlice) {
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		io.WriteString(w, ID)
+	}))
+	url, err := url.Parse(srv.URL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	trueVal := true
+	falseVal := false
+	portName := "http"
+	portNumber, err := strconv.ParseInt(url.Port(), 10, 32)
+	if err != nil {
+		log.Fatal(err)
+	}
+	portNumber32 := int32(portNumber)
+	endpointSlice := discoveryV1.EndpointSlice{
+		Endpoints: []discoveryV1.Endpoint{
+			{
+				Addresses: []string{url.Hostname()},
+				Conditions: discoveryV1.EndpointConditions{
+					Ready:       &trueVal,
+					Serving:     &trueVal,
+					Terminating: &falseVal,
+				},
+				TargetRef: &coreV1.ObjectReference{
+					UID: types.UID(ID),
+				},
+			},
+		},
+		Ports: []discoveryV1.EndpointPort{{Name: &portName, Port: &portNumber32}},
+	}
+	return srv, url, endpointSlice
+}
+
+func TestUpstreamSelection(t *testing.T) {
+	type TestCase struct {
+		RequestCookies          []http.Cookie
+		ProxyPort               int
+		UpstreamPortName        string
+		UpstreamIDs             []string
+		SessionCookieName       string
+		ExpectedResponseStatus  int
+		ExpectedResponseContent string
+		ExpectedResponseCookies map[string]string
+	}
+
+	runTest := func(t *testing.T, testCase TestCase) {
+		endpointItems := []EndpointStoreItem{}
+		endpointSlices := []*discoveryV1.EndpointSlice{}
+		for _, i := range testCase.UpstreamIDs {
+			server, _, endpoint := setupTestUpstream(i)
+			defer server.Close()
+			endpointSlices = append(endpointSlices, &endpoint)
+			endpointItems = append(endpointItems, NewEndpointStoreItems(&endpoint, testCase.UpstreamPortName)...)
+		}
+		store := NewEndpointStoreFromEndpointItems(endpointItems, false)
+		mockInformer := MockInformer{
+			mutex: sync.RWMutex{},
+			store: endpointSlices,
+		}
+		balancer := StickySessionBalancer{
+			ContainerPortName: testCase.UpstreamPortName,
+			CookieName:        testCase.SessionCookieName,
+			cache:             &mockInformer,
+			watcher:           &mockInformer,
+			store:             store,
+		}
+		e := echo.New()
+		proxy := middleware.ProxyWithConfig(middleware.ProxyConfig{
+			Balancer: &balancer,
+		})
+		e.Use(middleware.Logger())
+		e.Group("/").Use(proxy)
+		go e.Start(fmt.Sprintf(":%d", testCase.ProxyPort))
+		defer e.Close()
+		req, err := http.NewRequest("GET", fmt.Sprintf("http://localhost:%d/", testCase.ProxyPort), nil)
+		assert.NoError(t, err)
+		for _, c := range testCase.RequestCookies {
+			aCookie := c
+			req.AddCookie(&aCookie)
+		}
+		res, err := http.DefaultClient.Do(req)
+		assert.NoError(t, err)
+		assert.Equal(t, testCase.ExpectedResponseStatus, res.StatusCode)
+		resContent, err := io.ReadAll(res.Body)
+		defer res.Body.Close()
+		assert.NoError(t, err)
+		assert.Equal(t, testCase.ExpectedResponseContent, string(resContent))
+		assert.Equal(t, len(testCase.ExpectedResponseCookies), len(res.Cookies()))
+		cookiesMatched := 0
+		for expectedCookieName, expectedCookieVal := range testCase.ExpectedResponseCookies {
+			for _, actualCookie := range res.Cookies() {
+				if actualCookie.Name == expectedCookieName {
+					assert.Equal(t, expectedCookieVal, actualCookie.Value)
+					cookiesMatched++
+				}
+			}
+		}
+		assert.Equal(t, len(testCase.ExpectedResponseCookies), cookiesMatched)
+	}
+
+	testCases := []TestCase{
+		{
+			ProxyPort:               8080,
+			UpstreamPortName:        "http",
+			UpstreamIDs:             []string{"host1"},
+			RequestCookies:          []http.Cookie{},
+			SessionCookieName:       "session-cookie",
+			ExpectedResponseStatus:  http.StatusOK,
+			ExpectedResponseContent: "host1",
+			ExpectedResponseCookies: map[string]string{"session-cookie": "host1"},
+		},
+		{
+			ProxyPort:               8080,
+			UpstreamPortName:        "http",
+			UpstreamIDs:             []string{"host1", "host2"},
+			RequestCookies:          []http.Cookie{{Name: "session-cookie", Value: "host2"}},
+			SessionCookieName:       "session-cookie",
+			ExpectedResponseStatus:  http.StatusOK,
+			ExpectedResponseContent: "host2",
+			ExpectedResponseCookies: map[string]string{},
+		},
+		{
+			ProxyPort:               8080,
+			UpstreamPortName:        "http",
+			UpstreamIDs:             []string{"host1"},
+			RequestCookies:          []http.Cookie{{Name: "session-cookie", Value: "host2"}},
+			SessionCookieName:       "session-cookie",
+			ExpectedResponseStatus:  http.StatusOK,
+			ExpectedResponseContent: "host1",
+			ExpectedResponseCookies: map[string]string{"session-cookie": "host1"},
+		},
+		{
+			ProxyPort:               8080,
+			UpstreamPortName:        "http",
+			UpstreamIDs:             []string{},
+			RequestCookies:          []http.Cookie{},
+			SessionCookieName:       "session-cookie",
+			ExpectedResponseStatus:  http.StatusNotFound,
+			ExpectedResponseContent: "no upstream servers are available",
+			ExpectedResponseCookies: map[string]string{},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(fmt.Sprintf("%v", testCase), func(t *testing.T) {
+			runTest(t, testCase)
+		})
+	}
+}
+
+func TestEventHandling(t *testing.T) {
+	type EventType string
+	const (
+		Add    EventType = "add"
+		Delete EventType = "delete"
+		Update EventType = "update"
+	)
+	type Event struct {
+		Old  *discoveryV1.EndpointSlice
+		New  *discoveryV1.EndpointSlice
+		Type EventType
+	}
+	type TestCase struct {
+		UpstreamIDs       []string
+		Events            []Event
+		ExpectedEndpoints []EndpointStoreItem
+	}
+
+	runTest := func(t *testing.T, testCase TestCase) {
+		endpointItems := []EndpointStoreItem{}
+		endpointSlices := []*discoveryV1.EndpointSlice{}
+		for _, i := range testCase.UpstreamIDs {
+			server, _, endpoint := setupTestUpstream(i)
+			defer server.Close()
+			endpointSlices = append(endpointSlices, &endpoint)
+			endpointItems = append(endpointItems, NewEndpointStoreItems(&endpoint, "http")...)
+		}
+		store := NewEndpointStoreFromEndpointItems(endpointItems, false)
+		mockInformer := MockInformer{
+			mutex: sync.RWMutex{},
+			store: endpointSlices,
+		}
+		balancer := StickySessionBalancer{
+			ContainerPortName: "http",
+			cache:             &mockInformer,
+			watcher:           &mockInformer,
+			store:             store,
+		}
+		balancer.watcher.AddEventHandler(balancer)
+		for _, event := range testCase.Events {
+			switch event.Type {
+			case Add:
+				mockInformer.TriggerAdd(event.New)
+			case Delete:
+				mockInformer.TriggerDelete(event.Old)
+			case Update:
+				mockInformer.TriggerUpdate(event.Old, event.New)
+			}
+		}
+		assert.Equal(t, len(testCase.ExpectedEndpoints), len(balancer.store.List()))
+		for _, expectedEndpoint := range testCase.ExpectedEndpoints {
+			actualEndpoint, found := balancer.store.Get(expectedEndpoint.UID)
+			assert.True(t, found)
+			assert.Equal(t, expectedEndpoint.UID, actualEndpoint.UID)
+			assert.Equal(t, expectedEndpoint.Sessions, actualEndpoint.Sessions)
+		}
+	}
+
+	trueVal := true
+	falseVal := false
+	testCases := []TestCase{
+		{
+			UpstreamIDs:       []string{},
+			ExpectedEndpoints: []EndpointStoreItem{},
+		},
+		{
+			UpstreamIDs:       []string{"host1"},
+			ExpectedEndpoints: []EndpointStoreItem{{UID: "host1", Sessions: 0}},
+		},
+		{
+			UpstreamIDs:       []string{},
+			ExpectedEndpoints: []EndpointStoreItem{{UID: "host2", Sessions: 0}},
+			Events: []Event{
+				{
+					Type: Add,
+					New: &discoveryV1.EndpointSlice{
+						Endpoints: []discoveryV1.Endpoint{
+							{
+								Addresses: []string{"host2"},
+								TargetRef: &coreV1.ObjectReference{UID: types.UID("host2")},
+								Conditions: discoveryV1.EndpointConditions{
+									Ready:       &trueVal,
+									Serving:     &trueVal,
+									Terminating: &falseVal,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			UpstreamIDs:       []string{"host1"},
+			ExpectedEndpoints: []EndpointStoreItem{{UID: "host2", Sessions: 0}, {UID: "host1", Sessions: 0}},
+			Events: []Event{
+				{
+					Type: Add,
+					New: &discoveryV1.EndpointSlice{
+						Endpoints: []discoveryV1.Endpoint{
+							{
+								Addresses: []string{"host2"},
+								TargetRef: &coreV1.ObjectReference{UID: types.UID("host2")},
+								Conditions: discoveryV1.EndpointConditions{
+									Ready:       &trueVal,
+									Serving:     &trueVal,
+									Terminating: &falseVal,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			UpstreamIDs:       []string{"host1"},
+			ExpectedEndpoints: []EndpointStoreItem{},
+			Events: []Event{
+				{
+					Type: Delete,
+					Old: &discoveryV1.EndpointSlice{
+						Endpoints: []discoveryV1.Endpoint{
+							{
+								Addresses: []string{"host1"},
+								TargetRef: &coreV1.ObjectReference{UID: types.UID("host1")},
+								Conditions: discoveryV1.EndpointConditions{
+									Ready:       &trueVal,
+									Serving:     &trueVal,
+									Terminating: &falseVal,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			UpstreamIDs:       []string{"host1"},
+			ExpectedEndpoints: []EndpointStoreItem{},
+			Events: []Event{
+				{
+					Type: Update,
+					Old: &discoveryV1.EndpointSlice{
+						Endpoints: []discoveryV1.Endpoint{
+							{
+								Addresses: []string{"host1"},
+								TargetRef: &coreV1.ObjectReference{UID: types.UID("host1")},
+								Conditions: discoveryV1.EndpointConditions{
+									Ready:       &trueVal,
+									Serving:     &trueVal,
+									Terminating: &falseVal,
+								},
+							},
+						},
+					},
+					New: &discoveryV1.EndpointSlice{
+						Endpoints: []discoveryV1.Endpoint{
+							{
+								Addresses: []string{"host1"},
+								TargetRef: &coreV1.ObjectReference{UID: types.UID("host1")},
+								Conditions: discoveryV1.EndpointConditions{
+									Ready:       &falseVal,
+									Serving:     &trueVal,
+									Terminating: &falseVal,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(fmt.Sprintf("%v", testCase), func(t *testing.T) {
+			runTest(t, testCase)
+		})
+	}
+}
