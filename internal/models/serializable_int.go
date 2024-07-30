9f5fdb08942a3311c6a12d8b69587403e8eac625
Flora Thiebaut <flora.thiebaut@sdsc.ethz.ch>
2024-07-30 16:40:32 +0200

    try to fix marshalling

--- internal/models/serializable_int.go
+++ internal/models/serializable_int.go
@@ -1,29 +1,28 @@
 package models
 
 import (
-	"encoding/json"
 	"strconv"
 )
 
 type SerializableInt int
 
 func (s SerializableInt) MarshalBinary() (data []byte, err error) {
-	return json.Marshal(s)
-}
-
-func (s *SerializableInt) UnmarshalBinary(data []byte) error {
-	return json.Unmarshal(data, s)
-}
-
-func (s SerializableInt) MarshalText() (data []byte, err error) {
 	return []byte(strconv.Itoa(int(s))), nil
 }
 
-func (s *SerializableInt) UnmarshalText(data []byte) error {
+func (s *SerializableInt) UnmarshalBinary(data []byte) error {
 	val, err := strconv.Atoi(string(data))
 	if err != nil {
 		return err
 	}
 	*s = SerializableInt(val)
 	return nil
+}
+
+func (s SerializableInt) MarshalText() (data []byte, err error) {
+	return s.MarshalBinary()
+}
+
+func (s *SerializableInt) UnmarshalText(data []byte) error {
+	return s.UnmarshalBinary(data)
 }
