17955311f174544c5cc2ad60de22e446f6432dea
Flora Thiebaut <flora.thiebaut@sdsc.ethz.ch>
2024-10-10 09:23:35 +0200

    add tests

--- internal/config/session.go
+++ internal/config/session.go
@@ -19,14 +19,14 @@ type AuthorizationVerifier struct {
 	AuthorizedParty string
 }
 
-func (c *SessionConfig) Validate() error {
+func (c *SessionConfig) Validate(e RunningEnvironment) error {
 	if c.IdleSessionTTLSeconds <= 0 {
 		return fmt.Errorf("idle session TTL seconds (%d) needs to be greater than 0", c.IdleSessionTTLSeconds)
 	}
 	if c.MaxSessionTTLSeconds > 0 && c.IdleSessionTTLSeconds > c.MaxSessionTTLSeconds {
 		return fmt.Errorf("max session TTL seconds (%d) cannot be less than idle session TTL seconds (%d)", c.MaxSessionTTLSeconds, c.IdleSessionTTLSeconds)
 	}
-	if c.UnsafeNoCookieHandler {
+	if e != Development && c.UnsafeNoCookieHandler {
 		return fmt.Errorf("a cookie handler needs to be configured in production")
 	}
 	return nil
