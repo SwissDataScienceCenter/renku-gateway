4c1d5848b3b3928493ec321db44ff34792cd970f
Flora Thiebaut <flora.thiebaut@sdsc.ethz.ch>
2026-02-12 13:39:20 +0100

    exp: early sentry init

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -53,6 +53,22 @@ func main() {
 	e := echo.New()
 	e.Pre(middleware.RequestID(), middleware.RemoveTrailingSlash(), revproxy.UiServerPathRewrite())
 	e.Use(middleware.Recover())
+
+	// Sentry init
+	if gwConfig.Monitoring.Sentry.Enabled {
+		err := sentry.Init(sentry.ClientOptions{
+			Dsn:              string(gwConfig.Monitoring.Sentry.Dsn),
+			TracesSampleRate: gwConfig.Monitoring.Sentry.SampleRate,
+			Environment:      gwConfig.Monitoring.Sentry.Environment,
+		})
+		if err != nil {
+			slog.Error("sentry initialization failed", "error", err)
+		}
+		e.Use(sentryecho.New(sentryecho.Options{
+			Repanic: true,
+		}))
+	}
+
 	// The banner and the port do not respect the logger formatting we set below so we remove them
 	// the port will be logged further down when the server starts.
 	e.HideBanner = true
@@ -169,18 +185,6 @@ func main() {
 	// CORS
 	if len(gwConfig.Server.AllowOrigin) > 0 {
 		e.Use(middleware.CORSWithConfig(middleware.CORSConfig{AllowOrigins: gwConfig.Server.AllowOrigin}))
-	}
-	// Sentry
-	if gwConfig.Monitoring.Sentry.Enabled {
-		err := sentry.Init(sentry.ClientOptions{
-			Dsn:              string(gwConfig.Monitoring.Sentry.Dsn),
-			TracesSampleRate: gwConfig.Monitoring.Sentry.SampleRate,
-			Environment:      gwConfig.Monitoring.Sentry.Environment,
-		})
-		if err != nil {
-			slog.Error("sentry initialization failed", "error", err)
-		}
-		e.Use(sentryecho.New(sentryecho.Options{}))
 	}
 	// Prometheus
 	if gwConfig.Monitoring.Prometheus.Enabled {
